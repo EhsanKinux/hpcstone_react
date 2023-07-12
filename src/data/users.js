8b5a8afb32b4ab4b5d0a8b0d398e8174fb2b57f9
EhsanKINUX <ehsankhwentao@gmail.com>
2023-07-12 10:13:37 +0330

    Fix file data path

--- src/data/users.js
+++ src/data/users.js
@@ -2,7 +2,7 @@ export const users = [
     {
         id:1,
         logo:"",
-        avatar:"./manager.jpg",
+        avatar:"./src/assets/manager.jpg",
         comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         name:"Somaye Khodaveisi",
         title:"Manager",
@@ -10,7 +10,7 @@ export const users = [
     {
         id:2,
         logo:"",
-        avatar:"./webdeveloper.jpg",
+        avatar:"./src/assets/webdeveloper.jpg",
         comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         name:"Ehsan Khodaveisi",
         title:"Web Developer",
