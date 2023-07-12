6e15cd5eada69a419fec7417f7775e121717bf03
EhsanKINUX <ehsankhwentao@gmail.com>
2023-07-12 10:16:01 +0330

    Add circle component and fix path names

--- src/pages/mainPage/parts/Testimonials.jsx
+++ src/pages/mainPage/parts/Testimonials.jsx
@@ -1,9 +1,10 @@
+import Circle from "../../../components/Circle";
 import { users } from "../../../data/users";
 
 const Testimonials = () => {
   return (
     <div className="h-screen relative overflow-hidden mt-6 flex flex-col justify-around items-center mb-24">
-      {/* <Circle
+      <Circle
         backgroundColor="darkblue"
         top="-60vh"
         left="0"
@@ -11,17 +12,17 @@ const Testimonials = () => {
         margin="auto"
         width="100vh"
         height="100vh"
-      /> */}
+      />
       <h1 className="text-center font-bold text-4xl">تیم مشاور و پشتیبانی</h1>
       <div className="wrapper flex flex-wrap justify-around items-center">
         {users.map((user) => (
           <div key={user.id} className="card w-2/5">
-            <img src={`/${user.logo}`} width="30" height="30" alt="" />
+            <img src={user.logo} width="30" height="30" alt="" />
             <p className="comment text-xl font-light">{user.comment}</p>
             <div className="person flex items-center">
               <img
                 className="avatar rounded-full"
-                src={`/img/${user.avatar}`}
+                src={user.avatar}
                 width="45"
                 height="45"
                 alt=""
