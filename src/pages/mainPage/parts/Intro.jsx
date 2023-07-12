b645e3476a2b7ae404539a20924f32323e17cd64
Sina Moradian <sinacsharp@gmail.com>
2023-07-12 19:56:56 +0330

    Fix header image alignment and make it a perfect circle

--- src/pages/mainPage/parts/Intro.jsx
+++ src/pages/mainPage/parts/Intro.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./intro.css"
+import "./intro.css";
 
 const Intro = () => {
   return (
@@ -19,9 +19,9 @@ const Intro = () => {
           <p className="text-xl">Hard Precious Colorful</p>
         </div>
         <div>
-          <p className="text-2xl">
+          <h1 className="text-2xl">
             خرید سریع و مطمئن و ارزان مرغوب ترین سنگ های قروه
-          </p>
+          </h1>
         </div>
         <div>
           <Link
@@ -32,10 +32,10 @@ const Intro = () => {
           </Link>
         </div>
       </div>
-      <div className="flex-1 pr-0">
+      <div className="w-1/2">
         <img
           src="./src/assets/top-view-boards-mdf-material.jpg"
-          className="rounded-es-[100%] z-[100]"
+          className="rounded-es-[100%] z-[100] h-full object-cover"
           alt="img"
         />
         {/* <Circle
