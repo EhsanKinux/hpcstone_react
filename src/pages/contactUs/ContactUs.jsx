15eb01da8ffda4ccd740188be0f807a9ad865697
EhsanKINUX <ehsankhwentao@gmail.com>
2023-07-12 11:00:20 +0330

    Create contact us page

---
+++ src/pages/contactUs/ContactUs.jsx
@@ -0,0 +1,27 @@
+import RootLayout from "../../RootLayout"
+
+const ContactUs = () => {
+  return (
+    <RootLayout>
+    <div className="h-screen">
+      <div className="flex justify-center items-center gap-64 mt-10">
+        <div className="flex justify-center items-center" >
+          <img src='./src/assets/contactus.jpg' alt="contact" className="w-96" />
+        </div>
+        <div className="flex flex-col items-center">
+          <div>
+            <h3>ارتباط تلفنی و پیامکی</h3>
+            <span>+989189834232</span>
+          </div>
+          <div>
+            <h3>ایمیل</h3>
+            <span>info@hpcstone.com</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </RootLayout>
+  )
+}
+
+export default ContactUs
