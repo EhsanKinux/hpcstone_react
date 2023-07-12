1383220dccbd2fd3108036c8d5196d2e3ddbf36f
Sina Moradian <sinacsharp@gmail.com>
2023-07-12 19:19:50 +0330

    Add categories and base styles to the sidebar

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,33 +1,43 @@
 import { Link, Outlet } from "react-router-dom";
+import "./products.css";
 import RootLayout from "../../RootLayout";
 
 const Products = () => {
+  const categories = [
+    { id: 1, name: "بازالت", pathName: "bazalt" },
+    { id: 2, name: "گرانیت", pathName: "granite" },
+    { id: 3, name: "مرمر", pathName: "marble" },
+    { id: 4, name: "چینی", pathName: "porslain" },
+    { id: 5, name: "تراونیکس", pathName: "travenix" },
+    { id: 6, name: "تراورتن", pathName: "travertin" },
+  ];
+
   return (
     <RootLayout>
-      <h2>Products</h2>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/products/bazalt">بازالت</Link>
-          </li>
-          <li>
-            <Link to="/products/granite">گرانیت</Link>
-          </li>
-          <li>
-            <Link to="/products/marble">مرمر</Link>
-          </li>
-          <li>
-            <Link to="/products/porslain">چینی</Link>
-          </li>
-          <li>
-            <Link to="/products/travenix">تراونیکس</Link>
-          </li>
-          <li>
-            <Link to="/products/travertin">تراورتن</Link>
-          </li>
-        </ul>
-      </nav>
-      <Outlet /> {/* Renders the child routes */}
+      <div className="min-h-screen flex bg-white products">
+        <nav className="w-2/12 bg-slate-600 text-white">
+          <ul className="flex flex-col sticky top-20 gap-2 h-3/6 overflow-y-auto">
+            <h2 className="text-center mb-4">محصولات</h2>
+            {categories.map((category) => {
+              return (
+                <>
+                  <Link
+                    className="p-5 mx-5 rounded border border-amber-500"
+                    to={`/products/${category.pathName}`}
+                    key={category.id}
+                  >
+                    {category.name}
+                  </Link>
+                </>
+              );
+            })}
+          </ul>
+        </nav>
+        <div className="bg-gray-300 flex-1 h-[1080px]">
+          <Outlet />
+          {/* Renders the child routes */}
+        </div>
+      </div>
     </RootLayout>
   );
 };
