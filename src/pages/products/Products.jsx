e249e64d3410a958077a5d1d1e3d570bc310ca06
EhsanKINUX <ehsankhwentao@gmail.com>
2023-07-12 17:07:14 +0330

    Implementing the initial style of the products page

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,11 +1,35 @@
-import RootLayout from "../../RootLayout"
+import { Link, Outlet } from "react-router-dom";
+import RootLayout from "../../RootLayout";
 
 const Products = () => {
   return (
     <RootLayout>
-        <div>hello</div>
+      <h2>Products</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/products/bazalt">بازالت</Link>
+          </li>
+          <li>
+            <Link to="/products/granite">گرانیت</Link>
+          </li>
+          <li>
+            <Link to="/products/marble">مرمر</Link>
+          </li>
+          <li>
+            <Link to="/products/porslain">چینی</Link>
+          </li>
+          <li>
+            <Link to="/products/travenix">تراونیکس</Link>
+          </li>
+          <li>
+            <Link to="/products/travertin">تراورتن</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet /> {/* Renders the child routes */}
     </RootLayout>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
