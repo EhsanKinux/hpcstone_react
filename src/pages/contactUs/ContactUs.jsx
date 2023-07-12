a17f35b215e5cba39fca78c19a55b7b5b826468d
EhsanKINUX <ehsankhwentao@gmail.com>
2023-07-12 11:43:59 +0330

    Fix the contact us page bug

--- src/pages/contactUs/ContactUs.jsx
+++ src/pages/contactUs/ContactUs.jsx
@@ -1,27 +1,31 @@
-import RootLayout from "../../RootLayout"
+import RootLayout from "../../RootLayout";
 
 const ContactUs = () => {
   return (
     <RootLayout>
-    <div className="h-screen">
-      <div className="flex justify-center items-center gap-64 mt-10">
-        <div className="flex justify-center items-center" >
-          <img src='./src/assets/contactus.jpg' alt="contact" className="w-96" />
-        </div>
-        <div className="flex flex-col items-center">
-          <div>
-            <h3>ارتباط تلفنی و پیامکی</h3>
-            <span>+989189834232</span>
+      <div className="h-screen">
+        <div className="flex justify-center items-center gap-64 mt-10">
+          <div className="flex justify-center items-center">
+            <img
+              src="./src/assets/contactus.jpg"
+              alt="contact"
+              className="w-96"
+            />
           </div>
-          <div>
-            <h3>ایمیل</h3>
-            <span>info@hpcstone.com</span>
+          <div className="flex flex-col items-center">
+            <div>
+              <h3>ارتباط تلفنی و پیامکی</h3>
+              <span>+989189834232</span>
+            </div>
+            <div>
+              <h3>ایمیل</h3>
+              <span>info@hpcstone.com</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </RootLayout>
-  )
-}
+    </RootLayout>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
