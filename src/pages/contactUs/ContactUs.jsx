b52b160e3a9d30aebc5c321129e63c4dcd77ae5d
EhsanKINUX <ehsankhwentao@gmail.com>
2023-07-13 19:21:01 +0330

    Make changes in style and add Circle component in the contact us page

--- src/pages/contactUs/ContactUs.jsx
+++ src/pages/contactUs/ContactUs.jsx
@@ -1,15 +1,40 @@
 import RootLayout from "../../RootLayout";
+import Circle from "../../components/Circle";
 
 const ContactUs = () => {
   return (
     <RootLayout>
       <div className="h-screen">
-        <div className="flex justify-center items-center gap-64 mt-10">
-          <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center mt-10">
+          <Circle
+            backgroundColor="#b0ff49"
+            top="5vh"
+            left="-30vh"
+            width="100vh"
+            height="100vh"
+            // z-index="10"
+          />
+          <Circle
+            backgroundColor="#C7D36F"
+            top="5vh"
+            left="-50vh"
+            width="100vh"
+            height="100vh"
+            // z-index="10"
+          />
+          <Circle
+            backgroundColor="#9EB23B"
+            top="5vh"
+            left="-70vh"
+            width="100vh"
+            height="100vh"
+            // z-index="10"
+          />
+          <div className="flex justify-center items-center mt-20">
             <img
-              src="./src/assets/contactus.jpg"
+              src="./src/assets/ContactUs.png"
               alt="contact"
-              className="w-96"
+              className="w-2/4"
             />
           </div>
           <div className="flex flex-col items-center">
