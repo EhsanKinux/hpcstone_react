8bf648f2e7541785310e637c87211fa054ac571e
EhsanKINUX <ehsankhwentao@gmail.com>
2023-07-13 19:19:35 +0330

    Fix changes in the data

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 export const data = [
     {
         id:1,
-        name:"سنگ تراونیکس",
+        name:"travenix",
         title:"سنگ تراونیکس",
         desc:"با کیفت ترین سنگ های تراونیکس را از ما بخواهید",
         video:"./src/assets/ویدیو.mp4",
@@ -9,14 +9,14 @@ export const data = [
     },
     {
         id: 2,
-        name: "سنگ تراورتن",
+        name: "travertin",
         title: "سنگ تراورتن",
         desc: "با کیفیت ترین سنگ تراورتن را در ابعاد مختلف از ما بخواهید",
         photo: "./src/assets/تراورتن.jpg",
     },
     {
         id: 3,
-        name: "سنگ چینی",
+        name: "porslain",
         title: "سنگ چینی",
         desc: "ارائه با کیفیت ترین سنگ چینی قروه و سراسر ایران در ابعاد و اندازه های مختلف و به صورت کوپ",
         photo: "./src/assets/چینی.jpg",
